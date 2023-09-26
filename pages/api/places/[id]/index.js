7366767612a2f081c08b56bc2a9b948b3fd079d8
Gabriele Wittmann <gabrielewittmann@web.de>
2023-09-26 12:39:57 +0200

    read completed

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,17 +1,22 @@
-import { places } from "../../../../lib/db.js";
+import dbConnect from "@/db/dbConnect";
+import Place from "@/db/models/Place";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
+
   const { id } = request.query;
 
   if (!id) {
     return;
   }
 
-  const place = places.find((place) => place.id === id);
+  if (request.method === "GET") {
+    const place = await Place.findById(id);
 
-  if (!place) {
-    return response.status(404).json({ status: "Not found" });
+    if (!place) {
+      return response.status(404).json({ status: "Not found" });
+    }
+
+    response.status(200).json(place);
   }
-
-  response.status(200).json(place);
 }
